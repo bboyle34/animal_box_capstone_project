bee904dc6708d5b583462bdf1a580e65fea39759
Brendan Boyle <boylebj@dukes.jmu.edu>
2020-03-05 01:35:25 -0500

    Add files via upload
    create statement for Person and Pass

---
+++ SQL/PersonAndPass.sql
@@ -0,0 +1,19 @@
+--Create a database named "PBKDF2"
+DROP TABLE IF EXISTS Pass;
+DROP TABLE IF EXISTS Person;
+
+CREATE TABLE Person(
+UserID int IDENTITY (1,1) NOT NULL,
+Email nvarchar(30) NOT NULL,
+Address nvarchar(20) NOT NULL,
+Address2 nvarchar(30),
+City nvarchar (20) NOT NULL,
+State nvarchar (20),
+Zip nvarchar (20)
+PRIMARY KEY (UserID));
+
+CREATE TABLE Pass(
+UserID int FOREIGN KEY references Person(UserID) NOT NULL,
+Email nvarchar(30) NOT NULL,
+PasswordHash nvarchar(256) NOT NULL,
+PRIMARY KEY (UserID));
